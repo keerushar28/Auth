aaef794c038321ee79ef3c4a2555558348319c3f
Kiran Gautam <gautamkiran878@gmail.com>
2024-08-19 10:43:24 +0545

    Server Updated

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,19 @@
 import express from 'express'
 import dotenv from 'dotenv'
-dotenv.config();
 import connectDB from './DB/db.js';
-connectDB();
 import authRoutes from './routes/authRoutes.js'
 
-
+dotenv.config();
+connectDB();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.get("/", (req,res) => {
-    res.send("Hello World 123");
-})
-app.use('/api/auth',authRoutes)
+app.use(express.json()); 
+
+
+
+
+app.use('/api/auth', authRoutes)
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`
     )
